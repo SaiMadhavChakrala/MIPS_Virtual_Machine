1153f19ace87ba4de838d755b87228c11aebb253
SampathLgd66 <you@example.com>
2025-09-22 13:13:29 +0530

    Added mips_generator

--- Parser/src/mips_generator.cpp
+++ Parser/src/mips_generator.cpp
@@ -1,56 +1,124 @@
 #include "mips_generator.hpp"
 #include <fstream>
 #include <stdexcept>
-
 MipsGenerator::MipsGenerator(const std::vector<Instruction>& instructions) : instructions(instructions) {}
 
-void MipsGenerator::generate(const std::string& output_filename) {
+std::vector<std::string> MipsGenerator::generate(const std::string& output_filename) {
+    std::vector<std::string> assembly_lines;
     std::ofstream outfile(output_filename);
     if (!outfile.is_open()) {
         throw std::runtime_error("Could not open output file: " + output_filename);
     }
 
     // MIPS file header
-    outfile << ".data\n";
-    outfile << "newline: .asciiz \"\\n\"\n";
-    outfile << ".text\n";
-    outfile << ".globl main\n\n";
-    outfile << "main:\n";
+    assembly_lines.push_back(".data\n");
+    assembly_lines.push_back("newline: .asciiz \"\\n\"\n");
+    assembly_lines.push_back(".text\n");
+    assembly_lines.push_back(".globl main\n\n");
+    assembly_lines.push_back("main:\n");
 
     // Translate each instruction
+    
     for (const auto& instr : instructions) {
-        outfile << "    # " << instr.name << "\n";
+        assembly_lines.push_back("    # " + instr.name + "\n");
         if (instr.name == "ICONST") {
-            outfile << "    li $t0, " << instr.operands[0] << "\n";
-            outfile << "    sw $t0, 0($sp)\n";
-            outfile << "    addiu $sp, $sp, -4\n";
-        } else if (instr.name == "IADD") {
-            outfile << "    lw $t0, 4($sp)\n";
-            outfile << "    lw $t1, 8($sp)\n";
-            outfile << "    add $t0, $t0, $t1\n";
-            outfile << "    sw $t0, 8($sp)\n";
-            outfile << "    addiu $sp, $sp, 4\n";
-        } else if (instr.name == "PRINT") {
-            outfile << "    lw $a0, 4($sp)\n";
-            outfile << "    li $v0, 1\n";
-            outfile << "    syscall\n";
-            // Print a newline for better formatting
-            outfile << "    la $a0, newline\n";
-            outfile << "    li $v0, 4\n";
-            outfile << "    syscall\n";
-            outfile << "    addiu $sp, $sp, 4\n"; // Pop the value
-        } else if (instr.name == "POP") {
-            outfile << "    addiu $sp, $sp, 4\n";
-        } else if (instr.name == "RET") {
-            // In this simple case, RET will exit the program
-            outfile << "    li $v0, 10\n";
-            outfile << "    syscall\n";
+            std::string reg = reg_alloc.acquire();
+            assembly_lines.push_back("    li " + reg + ", " + std::to_string(instr.operands[0]) + "\n");
+            assembly_lines.push_back("    sw " + reg + ", 0($sp)\n");
+            assembly_lines.push_back("    addiu $sp, $sp, -4\n");
+            reg_alloc.release(reg);
+        } 
+        else if (instr.name == "IADD") {
+            std::string reg1 = reg_alloc.acquire();
+            std::string reg2 = reg_alloc.acquire();
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");
+            assembly_lines.push_back("    lw " + reg2 + ", 0($sp)\n"); // Pop second operand
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");
+            assembly_lines.push_back("    lw " + reg1 + ", 0($sp)\n"); // Pop first operand
+            assembly_lines.push_back("    add " + reg1 + ", " + reg1 + ", " + reg2 + "\n"); // reg1 = reg1 + reg2
+            assembly_lines.push_back("    sw " + reg1 + ", 0($sp)\n");   // Push result
+            assembly_lines.push_back("    addiu $sp, $sp, -4\n");
+            reg_alloc.release(reg2);
+            reg_alloc.release(reg1);
         }
-        // NOTE: INVOKE is not implemented in this simple generator
+        else if (instr.name == "ISUB") {
+            std::string reg1 = reg_alloc.acquire();
+            std::string reg2 = reg_alloc.acquire();
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");
+            assembly_lines.push_back("    lw " + reg2 + ", 0($sp)\n"); // Pop b
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");
+            assembly_lines.push_back("    lw " + reg1 + ", 0($sp)\n"); // Pop a
+            assembly_lines.push_back("    sub " + reg1 + ", " + reg1 + ", " + reg2 + "\n"); // reg1 = a - b
+            assembly_lines.push_back("    sw " + reg1 + ", 0($sp)\n");
+            assembly_lines.push_back("    addiu $sp, $sp, -4\n");
+            reg_alloc.release(reg2);
+            reg_alloc.release(reg1);
+        }
+        else if (instr.name == "IMUL") {
+            std::string reg1 = reg_alloc.acquire();
+            std::string reg2 = reg_alloc.acquire();
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");
+            assembly_lines.push_back("    lw " + reg2 + ", 0($sp)\n");
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");
+            assembly_lines.push_back("    lw " + reg1 + ", 0($sp)\n");
+            assembly_lines.push_back("    mul " + reg1 + ", " + reg1 + ", " + reg2 + "\n"); // reg1 = reg1 * reg2
+            assembly_lines.push_back("    sw " + reg1 + ", 0($sp)\n");
+            assembly_lines.push_back("    addiu $sp, $sp, -4\n");
+            reg_alloc.release(reg2);
+            reg_alloc.release(reg1);
+        }
+        else if (instr.name == "IDIV") {
+            std::string reg1 = reg_alloc.acquire();
+            std::string reg2 = reg_alloc.acquire();
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");
+            assembly_lines.push_back("    lw " + reg2 + ", 0($sp)\n"); // Pop divisor
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");
+            assembly_lines.push_back("    lw " + reg1 + ", 0($sp)\n"); // Pop dividend
+            assembly_lines.push_back("    div " + reg1 + ", " + reg2 + "\n");       // Lo = dividend / divisor
+            assembly_lines.push_back("    mflo " + reg1 + "\n");          // reg1 = Lo (quotient)
+            assembly_lines.push_back("    sw " + reg1 + ", 0($sp)\n");
+            assembly_lines.push_back("    addiu $sp, $sp, -4\n");
+            reg_alloc.release(reg2);
+            reg_alloc.release(reg1);
+        }
+        else if (instr.name == "ILT") {
+            std::string reg1 = reg_alloc.acquire();
+            std::string reg2 = reg_alloc.acquire();
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");
+            assembly_lines.push_back("    lw " + reg2 + ", 0($sp)\n"); // Pop b
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");
+            assembly_lines.push_back("    lw " + reg1 + ", 0($sp)\n"); // Pop a
+            assembly_lines.push_back("    slt " + reg1 + ", " + reg1 + ", " + reg2 + "\n"); // reg1 = (a < b) ? 1 : 0
+            assembly_lines.push_back("    sw " + reg1 + ", 0($sp)\n");
+            assembly_lines.push_back("    addiu $sp, $sp, -4\n");
+            reg_alloc.release(reg2);
+            reg_alloc.release(reg1);
+        }
+        else if (instr.name == "JMP") {
+            assembly_lines.push_back("    j L" + std::to_string(instr.operands[0]) + "\n"); // Unconditional jump to label
+        }
+        else if (instr.name == "PRINT") {
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n");   // Pop the value to print
+            assembly_lines.push_back("    lw $a0, 0($sp)\n");      // Load value into argument register $a0
+            assembly_lines.push_back("    li $v0, 1\n");         // Syscall code for printing integer
+            assembly_lines.push_back("    syscall\n");
+            // Print a newline for better formatting
+            assembly_lines.push_back("    la $a0, newline\n");
+            assembly_lines.push_back("    li $v0, 4\n");         // Syscall code for printing string
+            assembly_lines.push_back("    syscall\n");
+        } 
+        else if (instr.name == "POP") {
+            assembly_lines.push_back("    addiu $sp, $sp, 4\n"); // Discard top of stack by incrementing stack pointer
+        } 
+        else if (instr.name == "RET") {
+            assembly_lines.push_back("    li $v0, 10\n");        // Syscall code for exit
+            assembly_lines.push_back("    syscall\n");
+        }
     }
 
     // Default exit if no RET is found
-    outfile << "\n    # Default exit\n";
-    outfile << "    li $v0, 10\n";
-    outfile << "    syscall\n";
+    assembly_lines.push_back("\n    # Default exit\n");
+    assembly_lines.push_back("    li $v0, 10\n");
+    assembly_lines.push_back("    syscall\n");
+    return assembly_lines;
 }
--- Parser/src/mips_generator.hpp
+++ Parser/src/mips_generator.hpp
@@ -4,12 +4,12 @@
 #include "parser.hpp"
 #include <string>
 #include <vector>
-
+#include "register_allocator.hpp" // Include the new header
 class MipsGenerator {
 public:
     MipsGenerator(const std::vector<Instruction>& instructions);
-    void generate(const std::string& output_filename);
-
+    std::vector<std::string> generate(const std::string& output_filename);
+    RegisterAllocator reg_alloc;
 private:
     const std::vector<Instruction>& instructions;
 };
